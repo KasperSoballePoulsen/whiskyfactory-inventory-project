9c190576630c9858a33f18384e8c6e015b5f42a0
Kasper Søballe Poulsen <eaa23kapo@students.eaaa.dk>
2024-04-17 09:33:59 +0200

    hellpo

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello worl");
         System.out.println("mine gruppe medlemmer lugter");
+
+        System.out.println("yoooo");
     }
 }
