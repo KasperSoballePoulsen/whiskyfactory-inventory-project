26aed680a1780607dfa713745bda140da9848660
Noah Kristensen <noahkristensen1@gmail.com>
2024-04-17 09:21:44 +0200

    wow

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("wow");
+        System.out.println("hello worl");
     }
 }
