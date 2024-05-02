23e7231d03f151446678c55e24d79146be7fbe4f
Noah Kristensen <noahkristensen1@gmail.com>
2024-05-02 12:48:06 +0200

    tilføj test

---
+++ test/application/model/FadTest.java
@@ -0,0 +1,16 @@
+package application.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FadTest {
+
+    @Test
+    void paafyld() {
+    }
+
+    @Test
+    void aftap() {
+    }
+}
