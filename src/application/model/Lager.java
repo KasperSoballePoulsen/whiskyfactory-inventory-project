a809c7468b838570ff3905f7d0b27398ba7bba63
Noah Kristensen <noahkristensen1@gmail.com>
2024-05-06 12:57:31 +0200

    Constructor af Fad i lager

--- src/application/model/Lager.java
+++ src/application/model/Lager.java
@@ -1,5 +1,7 @@
 package application.model;
 
+import java.util.NoSuchElementException;
+
 public class Lager {
     private Fad[] pladser;
 
@@ -16,5 +18,29 @@ public class Lager {
 
     public String getNavn() {
         return navn;
+    }
+    public Fad createFad(int nummer,String type, int stoerrelse, String leverandør){
+        Fad fad = new Fad(nummer,type,stoerrelse,this,leverandør);
+        int pladsNr = findFørsteLedigePlads();
+        pladser[pladsNr] = fad;
+        return fad;
+    }
+
+    public int findFørsteLedigePlads(){
+        boolean found = false;
+        int i = 0;
+        int k = 0;
+        while (!found && i < pladser.length){
+             k = i;
+            if (pladser[k] == null){
+                found = true;
+            }
+            i++;
+        }
+        if (found) {
+            return k;
+        } else {
+         throw new IndexOutOfBoundsException("lager er fyldt");
+        }
     }
 }
