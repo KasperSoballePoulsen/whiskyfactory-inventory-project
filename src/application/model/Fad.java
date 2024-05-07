7b6e19d523b4de0d59c644e7ad61806193c6d575
Noah Kristensen <noahkristensen1@gmail.com>
2024-05-07 09:58:37 +0200

    opdateret fad

--- src/application/model/Fad.java
+++ src/application/model/Fad.java
@@ -2,9 +2,7 @@ package application.model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Fad {
     private int nummer;
@@ -17,7 +15,7 @@ public class Fad {
     private LocalDate slutdato;
 
     private Destilat destilat;
-    private List<Medarbejder> medarbejderer;
+    private Medarbejder paafylder;
 
 
     public Fad(int nummer, String type, int stoerrelse, Lager lager, String leverandoer) {
@@ -29,7 +27,7 @@ public class Fad {
 
         this.leverandoer = leverandoer;
 
-        medarbejderer = new ArrayList<>();
+        paafylder = new ArrayList<>();
     }
 
     public int getNummer() {
@@ -55,7 +53,6 @@ public class Fad {
 
     public String getLeverandoer() {
         return leverandoer;
-
     }
 
     public LocalDate getStartdato() {
@@ -71,26 +68,20 @@ public class Fad {
     }
 
     public Medarbejder getPaafylder() {
-        return medarbejderer.get(0);
-    }
-
-    public List<Medarbejder> getAftapper() {
-        return medarbejderer.subList(1, medarbejderer.size());
+        return paafylder;
     }
 
     public void paafyld(Medarbejder medarbejder, Destilat destilat, LocalDate startdato, int antalLiterPaafyldt) {
         if (this.destilat == null) {
-            medarbejderer.set(0,medarbejder);
+            this.paafylder = medarbejder;
             this.destilat = destilat;
             this.startdato = startdato;
             this.antalLiterPaafyldt = antalLiterPaafyldt;
         }
     }
 
-    public void aftap(Medarbejder medarbejder, int literTapet, LocalDate slutdato) {
+    public void aftap(int literTapet) {
         if (literTapet <= antalLiterPaafyldt) {
-            this.slutdato = slutdato;
-            medarbejderer.add(medarbejder);
             antalLiterPaafyldt -= literTapet;
         } else {
             throw new IllegalArgumentException("der er ikke nok liter");
