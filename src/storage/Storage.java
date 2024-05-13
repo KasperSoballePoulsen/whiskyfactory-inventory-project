0d7d32fdae33d1163498ec5d9f33f9e30c8feced
Rasmus <158032188+Frizzybusy@users.noreply.github.com>
2024-05-13 08:59:09 +0200

    fixed noahs storage >:)

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -8,43 +8,43 @@ import java.util.List;
 public class Storage {
 
 
-    private final List<Lager> lagerer = new ArrayList<>();
-    private final List<Destilat> destilater = new ArrayList<>();
-    private final List<Tapning> tapninger = new ArrayList<>();
-    private final List<Paafyldning> paafyldninger = new ArrayList<>();
+    private static List<Lager> lagerer = new ArrayList<>();
+    private static List<Destilat> destilater = new ArrayList<>();
+    private static List<Tapning> tapninger = new ArrayList<>();
+    private static List<Paafyldning> paafyldninger = new ArrayList<>();
 
 
-    public void addLager(Lager lager) {
+    public static void addLager(Lager lager) {
         lagerer.add(lager);
     }
 
 
-    public void addDestilat(Destilat destilat) {
+    public static void addDestilat(Destilat destilat) {
         destilater.add(destilat);
     }
 
-    public void addPaafyldning(Paafyldning paafyldning) {
+    public static void addPaafyldning(Paafyldning paafyldning) {
         paafyldninger.add(paafyldning);
     }
 
-    public void addTapning(Tapning tapning) {
+    public static void addTapning(Tapning tapning) {
         tapninger.add(tapning);
     }
 
-    public List<Lager> getLagerer() {
+    public static List<Lager> getLagerer() {
         return new ArrayList<>(lagerer);
     }
 
 
-    public List<Destilat> getDestilater() {
+    public static List<Destilat> getDestilater() {
         return new ArrayList<>(destilater);
     }
 
-    public List<Tapning> getTapninger() {
+    public static List<Tapning> getTapninger() {
         return new ArrayList<>(tapninger);
     }
 
-    public List<Paafyldning> getPaafyldninger() {
+    public static List<Paafyldning> getPaafyldninger() {
         return new ArrayList<>(paafyldninger);
     }
 }
